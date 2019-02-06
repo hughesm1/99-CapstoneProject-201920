c16511349f46247fc5ffc44d357d80cefa54b408
hughesm1 <hughesm1@rose-hulman.edu>
2019-02-06 10:38:03 -0500

    name in m1

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -3,7 +3,7 @@
   This code is the delegate for handling messages from the shared GUI.
 
   Author:  Your professors (for the framework)
-    and PUT_YOUR_NAMES_HERE.
+    and Marcus Hughes-Oliver, Zachary Juday, Grant Stewart.
   Winter term, 2018-2019.
 """
 
