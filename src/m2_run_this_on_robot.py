e411c1fd389a386834ccedd9562874c360bc668c
stewargb <stewargb@rose-hulman.edu>
2019-02-06 11:30:28 -0500

    test code update

--- src/m2_run_this_on_robot.py
+++ src/m2_run_this_on_robot.py
@@ -16,11 +16,18 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
-    run_test_arm()
+    #run_test_arm_raise()
+    run_test_arm_calibrate()
 
-def run_test_arm():
+
+def run_test_arm_raise():
     robot=rosebot.RoseBot()
     robot.arm_and_claw.raise_arm()
+
+def run_test_arm_calibrate():
+    robot=rosebot.RoseBot()
+    robot.arm_and_claw.calibrate_arm()
+
 
 
 # -----------------------------------------------------------------------------
