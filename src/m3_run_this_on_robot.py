741da21c3d77520127676a36f5c157f4f7bf4a1f
zjuday <judayzj@rose-hulman.edu>
2019-02-07 11:14:28 -0500

    defined go in rosebot

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -16,7 +16,16 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
+    real_thing()
 
+def real_thing():
+    robot = rosebot.RoseBot()
+    delegate = shared_gui_delegate_on_robot.Handler(robot)
+    mqtt_reciever = com.MqttClient(delegate)
+    mqtt_reciever.connect_to_pc()
+
+    while True:
+        time.sleep(0.01)
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
