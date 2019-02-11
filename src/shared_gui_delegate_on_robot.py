876aede563f0d7bef5f509ce54ed89228ef300df
hughesm1 <hughesm1@rose-hulman.edu>
2019-02-11 10:54:59 -0500

    shared gui stuff

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -83,7 +83,7 @@ class Handler(object):
 
     def tone(self, freq, dur):
         print('got tone')
-        self.robot.sound_system.tone_maker.play_tone(int(freq), int(dur)).wait(int(dur) * 1000)
+        self.robot.sound_system.tone_maker.play_tone(int(freq), int(dur) * 1000).wait(int(dur) * 1000)
 
     def speak(self, string):
         print('got speak')
